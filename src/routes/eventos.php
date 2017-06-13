76df00cabe641bffbe7ba16b8068c0390809670a
HenriqueSilva2 <joaohenriquesilva@ua.pt>
2017-06-13 20:32:27 +0100

    ordenacao e paginacao no GET dos participantes

--- src/routes/eventos.php
+++ src/routes/eventos.php
@@ -45,10 +45,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 #   results = número de resultados por página
 #   min: 1, max: 10
 # Exemplo: /api/eventos?page=1&results=2&by=id&order=ASC
-
 $app->get('/api/eventos', function (Request $request, Response $response) {
-
-    // Todo verificação dos parametros
 
     $byArr = [
         'id' => 'id_eventos',
@@ -170,6 +167,8 @@ $app->get('/api/eventos', function (Request $request, Response $response) {
     }
 });
 
+
+//////////// Obter todos os dados sobre um evento através do id ////////////
 $app->get('/api/eventos/{id}', function (Request $request, Response $response) {
     $id = (int)$request->getAttribute('id'); // ir buscar id
     //verificar se é um id válido
@@ -246,11 +245,63 @@ $app->get('/api/eventos/{id}', function (Request $request, Response $response) {
     }
 });
 
+//////////// Obter todos os eventos ////////////
+# Parametros:
+#   *page = pagina de resultados *obrigatorio
+#   results = número de resultados por página
+#   min: 1, max: 10
+# Exemplo: /api/eventos/2/utilizadores?page=1&results=2&by=id&order=ASC
 $app->get('/api/eventos/{id}/utilizadores', function (Request $request, Response $response) {
-    $id = (int)$request->getAttribute('id'); // ir buscar id a querystring
+    $id = (int)$request->getAttribute('id'); // ir buscar id
 
-    if (is_int($id) && $id > 0) {
-        $sql = "SELECT id_estatutos,nome_estatuto, `id_utilizadores`,`nome`,`apelido`,`foto`,`sobre_mini`,`telemovel` FROM `utilizadores` INNER JOIN estatutos ON utilizadores.`estatutos_id_estatutos` = estatutos.id_estatutos INNER JOIN participantes ON utilizadores.id_utilizadores = participantes.utilizadores_id_utilizadores WHERE eventos_id_eventos = :id";
+    $byArr = [
+        'id' => 'id_utilizadores',
+        'nome' => 'nome_evento',
+        'dataRegisto' => 'data_registo_evento',
+        'dataEvento' => 'data_evento',
+        'dataFim' => 'data_fim',
+        'ativo' => 'ativo',
+        'tipoEvento' => 'nome_tipo_evento',
+        'local' => 'nome'
+    ]; // Valores para ordernar por, fizemos uma array para simplificar queries
+
+
+    //valores default
+    $maxResults = 10; // maximo de resultados por pagina
+    $minResults = 1; // minimo de resultados por pagina
+    $byDefault = 'id'; // order by predefinido
+    $paginaDefault = 1; // pagina predefenida
+    $orderDefault = "DESC"; //ordenação predefenida
+
+
+    $parametros = $request->getQueryParams(); // obter parametros do querystring
+    $page = isset($parametros['page']) ? (int)$parametros['page'] : $paginaDefault;
+    $results = isset($parametros['results']) ? (int)$parametros['results'] : $maxResults;
+    $by = isset($parametros['by']) ? $parametros['by'] : $byDefault;
+    $order = isset($parametros['order']) ? $parametros['order'] : $orderDefault;
+
+
+    if (is_int($id) && $id > 0 && $page > 0 && $results > 0) {
+
+        //caso request tenha parametros superiores ao numero máximo permitido então repor com o valor maximo permitido e vice-versa
+        $results = $results > $maxResults ? $maxResults : $results; //se o querystring results for maior que o valor maximo definido passa a ser esse valor maximo definido
+        $results = $results < $minResults ? $minResults : $results; //se o querystring results for menor que o valor minimo definido passa a ser esse valor minimo definido
+        //caso tenha parametros diferentes de "ASC" ou "DESC" então repor com o predefinido
+        $order = $order == "ASC" || $order == "DESC" ? $order : $orderDefault;
+        //order by se existe como key no array, caso nao repor com o predefenido
+        $by = array_key_exists($by, $byArr) ? $by : $byDefault;
+        //A partir de quando seleciona resultados
+        $limitNumber = ($page - 1) * $results;
+        $passar = $byArr[$by];
+
+        //Apenas informações básicas dos inscritos para por exemplo cards, e ao clicar na card o utilizador pode ser reencaminhado para o masterdetail do utlizador da card
+        if ($order == $orderDefault) {
+            $sql = "SELECT id_estatutos,nome_estatuto, `id_utilizadores`,`nome`,`apelido`,`foto`,`sobre_mini`,`telemovel` FROM `utilizadores` INNER JOIN estatutos ON utilizadores.`estatutos_id_estatutos` = estatutos.id_estatutos INNER JOIN participantes ON utilizadores.id_utilizadores = participantes.utilizadores_id_utilizadores WHERE eventos_id_eventos = :id ORDER BY $passar DESC LIMIT :limit , :results";
+        } else {
+            $sql = "SELECT id_estatutos,nome_estatuto, `id_utilizadores`,`nome`,`apelido`,`foto`,`sobre_mini`,`telemovel` FROM `utilizadores` INNER JOIN estatutos ON utilizadores.`estatutos_id_estatutos` = estatutos.id_estatutos INNER JOIN participantes ON utilizadores.id_utilizadores = participantes.utilizadores_id_utilizadores WHERE eventos_id_eventos = :id ORDER BY $passar  LIMIT :limit , :results";
+
+        }
+
 
         try {
             $status = 200; // OK
@@ -262,6 +313,8 @@ $app->get('/api/eventos/{id}/utilizadores', function (Request $request, Response
             $db = $db->connect();
             $stmt = $db->prepare($sql);
             $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $stmt->bindValue(':limit', (int)$limitNumber, PDO::PARAM_INT);
+            $stmt->bindValue(':results', (int)$results, PDO::PARAM_INT);
             $stmt->execute();
             $db = null;
             $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -277,8 +330,15 @@ $app->get('/api/eventos/{id}/utilizadores', function (Request $request, Response
             $dadosLength = (int)sizeof($dados);
 
             if ($dadosLength === 0) {
-                $dados = ["error" => 'participantes inexistentes'];
+                $dados = ["error" => 'participantes/página inexistentes'];
                 $status = 404; // Page not found
+            } else if ($dadosLength < $results) {
+                $dadosExtra = ['info' => 'final dos resultados'];
+                array_push($dados, $dadosExtra);
+            } else {
+                $nextPageUrl = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+                $dadosExtra = ['proxPagina' => "$nextPageUrl?page=" . ++$page . "&results=$results"];
+                array_push($dados, $dadosExtra);
             }
 
             $responseData = [
@@ -321,3 +381,4 @@ $app->get('/api/eventos/{id}/utilizadores', function (Request $request, Response
 
 
 });
+
