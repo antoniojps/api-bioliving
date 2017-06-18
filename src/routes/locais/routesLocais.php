908095051da9beedc2048e3ac13330188550c5a4
HenriqueSilva2 <joaohenriquesilva@ua.pt>
2017-06-18 06:47:43 +0100

    GET dos locais e pesquisa por locais

--- src/routes/locais/routesLocais.php
+++ src/routes/locais/routesLocais.php
@@ -2,3 +2,4 @@
 require "../src/routes/locais/postLocais.php";
 require "../src/routes/locais/putLocais.php";
 require "../src/routes/locais/deleteLocais.php";
+require "../src/routes/locais/getLocais.php";
