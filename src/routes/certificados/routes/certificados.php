5760961a0961e161d70a7fb1174c475b6c6348d4
HenriqueSilva2 <joaohenriquesilva@ua.pt>
2017-06-26 01:14:04 +0100

    atualizacoes

--- src/routes/certificados/routes/certificados.php
+++ src/routes/certificados/routes/certificados.php
@@ -404,7 +404,7 @@ INNER  JOIN eventos ON eventos.id_eventos = validaparticipantes.eventos_id_event
 LEFT OUTER JOIN tipo_evento ON tipo_evento.id_tipo_evento = eventos.tipo_evento_id_tipo_evento 
 LEFT OUTER JOIN localizacao ON localizacao.localizacao = eventos.localizacao_localizacao 
 LEFT OUTER JOIN icons ON icons.id_icons = tipo_evento.icons_id 
-LEFT OUTER JOIN interesses ON interesses.eventos_id_eventos = eventos.id_eventos
+LEFT OUTER JOIN validaparticipantes ON validaparticipantes.eventos_id_eventos = eventos.id_eventos
 WHERE validaparticipantes.utilizadores_id_utilizadores <> :id AND validaparticipantes.eventos_id_eventos NOT IN (SELECT `eventos_id_eventos` FROM validaparticipantes WHERE validaparticipantes.`utilizadores_id_utilizadores` = :id ) ORDER BY $passar  LIMIT :limit , :results";
                 } else {
                     $sql = "SELECT  DISTINCT validaparticipantes.eventos_id_eventos,eventos.nome_evento,eventos.descricao_short,eventos.descricao,eventos.utilizadores_id_utilizadores AS criador,eventos.facebook_id,eventos.data_evento, tipo_evento.nome_tipo_evento,localizacao.nome,localizacao.lng,localizacao.lat,icons.classe AS tipo_classe 
@@ -413,7 +413,7 @@ INNER  JOIN eventos ON eventos.id_eventos = validaparticipantes.eventos_id_event
 LEFT OUTER JOIN tipo_evento ON tipo_evento.id_tipo_evento = eventos.tipo_evento_id_tipo_evento 
 LEFT OUTER JOIN localizacao ON localizacao.localizacao = eventos.localizacao_localizacao 
 LEFT OUTER JOIN icons ON icons.id_icons = tipo_evento.icons_id 
-LEFT OUTER JOIN interesses ON interesses.eventos_id_eventos = eventos.id_eventos
+LEFT OUTER JOIN validaparticipantes ON validaparticipantes.eventos_id_eventos = eventos.id_eventos
 WHERE validaparticipantes.utilizadores_id_utilizadores <> :id AND validaparticipantes.eventos_id_eventos NOT IN (SELECT `eventos_id_eventos` FROM validaparticipantes WHERE validaparticipantes.`utilizadores_id_utilizadores` = :id ) ORDER BY $passar DESC LIMIT :limit , :results";
                 }
             } else {
@@ -424,7 +424,7 @@ INNER  JOIN eventos ON eventos.id_eventos = validaparticipantes.eventos_id_event
 LEFT OUTER JOIN tipo_evento ON tipo_evento.id_tipo_evento = eventos.tipo_evento_id_tipo_evento 
 LEFT OUTER JOIN localizacao ON localizacao.localizacao = eventos.localizacao_localizacao 
 LEFT OUTER JOIN icons ON icons.id_icons = tipo_evento.icons_id 
-LEFT OUTER JOIN interesses ON interesses.eventos_id_eventos = eventos.id_eventos
+LEFT OUTER JOIN validaparticipantes ON validaparticipantes.eventos_id_eventos = eventos.id_eventos
 WHERE validaparticipantes.utilizadores_id_utilizadores = :id ORDER BY $passar  LIMIT :limit , :results";
                 } else {
                     $sql = "SELECT  DISTINCT validaparticipantes.eventos_id_eventos,eventos.nome_evento,eventos.descricao_short,eventos.descricao,eventos.utilizadores_id_utilizadores AS criador,eventos.facebook_id,eventos.data_evento, tipo_evento.nome_tipo_evento,localizacao.nome,localizacao.lng,localizacao.lat,icons.classe AS tipo_classe 
@@ -433,8 +433,8 @@ INNER  JOIN eventos ON eventos.id_eventos = validaparticipantes.eventos_id_event
 LEFT OUTER JOIN tipo_evento ON tipo_evento.id_tipo_evento = eventos.tipo_evento_id_tipo_evento 
 LEFT OUTER JOIN localizacao ON localizacao.localizacao = eventos.localizacao_localizacao 
 LEFT OUTER JOIN icons ON icons.id_icons = tipo_evento.icons_id 
-LEFT OUTER JOIN interesses ON interesses.eventos_id_eventos = eventos.id_eventos
-WHERE validaparticipantes.utilizadores_id_utilizadores = :id ORDER BY $passar DESC LIMIT :limit , :results";
+LEFT OUTER JOIN validaparticipantes ON validaparticipantes.eventos_id_eventos = eventos.id_eventos
+WHERE valida.utilizadores_id_utilizadores = :id ORDER BY $passar DESC LIMIT :limit , :results";
                 }
             }
 
@@ -820,4 +820,139 @@ $app->delete('/certificados/utilizador/{idUtilizador}/evento/{idEvento}', functi
         return $response
             ->withJson($errorMsg, $status, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
     }
+});
+
+
+$app->get('/eventos/{id}/certificados', function (Request $request, Response $response) {
+    $id = (int)$request->getAttribute('id'); // ir buscar id
+
+    $byArr = [
+        'id' => 'id_utilizadores',
+        'nome' => 'nome',
+        'nomeEstatuto' => 'nome_estatuto'
+    ]; // Valores para ordernar por, fizemos uma array para simplificar queries
+
+
+    //valores default
+    $maxResults = 10; // maximo de resultados por pagina
+    $minResults = 1; // minimo de resultados por pagina
+    $byDefault = 'id'; // order by predefinido
+    $paginaDefault = 1; // pagina predefenida
+    $orderDefault = "ASC"; //ordenação predefenida
+
+
+    $parametros = $request->getQueryParams(); // obter parametros do querystring
+    $page = isset($parametros['page']) ? (int)$parametros['page'] : $paginaDefault;
+    $results = isset($parametros['results']) ? (int)$parametros['results'] : $maxResults;
+    $by = isset($parametros['by']) ? $parametros['by'] : $byDefault;
+    $order = isset($parametros['order']) ? $parametros['order'] : $orderDefault;
+
+
+    if (is_int($id) && $id > 0 && $page > 0 && $results > 0) {
+
+        //caso request tenha parametros superiores ao numero máximo permitido então repor com o valor maximo permitido e vice-versa
+        $results = $results > $maxResults ? $maxResults : $results; //se o querystring results for maior que o valor maximo definido passa a ser esse valor maximo definido
+        $results = $results < $minResults ? $minResults : $results; //se o querystring results for menor que o valor minimo definido passa a ser esse valor minimo definido
+        //caso tenha parametros diferentes de "ASC" ou "DESC" então repor com o predefinido
+        $order = $order == "ASC" || $order == "DESC" ? $order : $orderDefault;
+        //order by se existe como key no array, caso nao repor com o predefenido
+        $by = array_key_exists($by, $byArr) ? $by : $byDefault;
+        //A partir de quando seleciona resultados
+        $limitNumber = ($page - 1) * $results;
+        $passar = $byArr[$by];
+
+        //Apenas informações básicas dos inscritos para por exemplo cards, e ao clicar na card o utilizador pode ser reencaminhado para o masterdetail do utlizador da card
+        if ($order == $orderDefault) {
+            $sql = "SELECT id_estatutos,nome_estatuto, `id_utilizadores`,`nome`,`apelido`,`foto`,`sobre_mini`,`telemovel` FROM `utilizadores` INNER JOIN estatutos ON utilizadores.`estatutos_id_estatutos` = estatutos.id_estatutos INNER JOIN validaparticipantes ON utilizadores.id_utilizadores = validaparticipantes.utilizadores_id_utilizadores WHERE eventos_id_eventos = :id ORDER BY $passar  LIMIT :limit , :results";
+        } else {
+            $sql = "SELECT id_estatutos,nome_estatuto, `id_utilizadores`,`nome`,`apelido`,`foto`,`sobre_mini`,`telemovel` FROM `utilizadores` INNER JOIN estatutos ON utilizadores.`estatutos_id_estatutos` = estatutos.id_estatutos INNER JOIN validaparticipantes ON utilizadores.id_utilizadores = validaparticipantes.utilizadores_id_utilizadores WHERE eventos_id_eventos = :id ORDER BY $passar DESC LIMIT :limit , :results";
+
+        }
+
+
+        try {
+            $status = 200; // OK
+
+            // iniciar ligação à base de dados
+            $db = new Db();
+
+            // conectar
+            $db = $db->connect();
+            $stmt = $db->prepare($sql);
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $stmt->bindValue(':limit', (int)$limitNumber, PDO::PARAM_INT);
+            $stmt->bindValue(':results', (int)$results, PDO::PARAM_INT);
+            $stmt->execute();
+            $db = null;
+            $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            // remover nulls e strings vazias
+            $dados = array_filter(array_map(function ($evento) {
+                return $evento = array_filter($evento, function ($coluna) {
+                    return $coluna !== null && $coluna !== '';
+                });
+            }, $dados));
+
+
+            $dadosLength = (int)sizeof($dados);
+
+            if ($dadosLength === 0) {
+                $responseData = [
+                    "status" => 404,
+                    "info" => 'pagina inexistente'
+                ]; // Page not found
+            } else if ($dadosLength < $results) {
+                $responseData = [
+                    "status" => 200,
+                    "data" => $dados,
+                    "info" => "final dos resultados"
+                ];
+            } else {
+                $nextPageUrl = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+                $responseData = [
+                    "status" => 200,
+                    "data" => $dados,
+                    "proxPagina" => "$nextPageUrl?page=" . ++$page . "&results=$results"
+                ];
+            }
+
+
+            return $response
+                ->withJson($responseData, $status, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
+
+
+        } catch (PDOException $err) {
+            $status = 503; // Service unavailable
+
+            // Primeiro callback chamado em ambiente de desenvolvimento, segundo em producao
+            $errorMsg = Errors::filtroReturn(function ($err) {
+                return [
+                    "status" => $err->getCode(),
+                    "info" => $err->getMessage()
+                ];
+            }, function () {
+                return [
+                    "status" => 503,
+                    "info" => "Servico Indisponive"
+                ];
+            }, $err);
+
+            return $response
+                ->withJson($errorMsg, $status, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
+        }
+    } else {
+        $status = 422; // Unprocessable Entity
+        $errorMsg = [
+
+            "status" => "$status",
+            "info" => 'Parametros invalidos'
+
+
+        ];
+
+        return $response
+            ->withJson($errorMsg, $status, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
+    }
+
+
 });
