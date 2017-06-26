b439315440016d612ce27ed82e4e962e83ba2262
HenriqueSilva2 <joaohenriquesilva@ua.pt>
2017-06-26 18:15:19 +0100

    GET tags

--- src/routes/tipos/getTipoEventos.php
+++ src/routes/tipos/getTipoEventos.php
@@ -149,3 +149,114 @@ $app->get('/tiposEventos', function (Request $request, Response $response) {
             ->withJson($errorMsg, $status, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
     }
 });
+$app->get('/icones', function (Request $request, Response $response) {
+    $maxResults = 10; // maximo de resultados por pagina
+    $minResults = 1;
+    $paginaDefault = 1;
+    $msgDefault = "%";
+
+    $parametros = $request->getQueryParams(); // obter parametros do querystring
+    $page = isset($parametros['page']) ? (int)$parametros['page'] : $paginaDefault;
+    $results = isset($parametros['results']) ? (int)$parametros['results'] : $maxResults;
+    $msg = isset($parametros['msg']) ? $parametros['msg'] : $msgDefault;
+
+
+    if ($page > 0 && $results > 0) {
+
+        //definir numero de resultados
+        //caso request tenha parametros superiores ao numero máximo permitido então repor com o valor maximo permitido e vice-versa
+        $results = $results > $maxResults ? $maxResults : $results; //se o querystring results for maior que o valor maximo definido passa a ser esse valor maximo definido
+        $results = $results < $minResults ? $minResults : $results; //se o querystring results for menor que o valor minimo definido passa a ser esse valor minimo definido
+        //caso tenha parametros diferentes de "ASC" ou "DESC" então repor com o predefinido
+
+        // A partir de quando seleciona resultados
+        $limitNumber = ($page - 1) * $results;
+
+        $sql = "SELECT * FROM tags WHERE tag_nome LIKE :msg  LIMIT :limit , :results";
+
+        try {
+            $responseData = "";
+            $status = 200; // OK
+            // iniciar ligação à base de dados
+            $db = new Db();
+            $msgEnv = "%$msg%";
+            // conectar
+            $db = $db->connect();
+            $stmt = $db->prepare($sql);
+            $stmt->bindValue(':limit', (int)$limitNumber, PDO::PARAM_INT);
+            $stmt->bindValue(':results', (int)$results, PDO::PARAM_INT);
+            $stmt->bindValue(':msg', $msgEnv, PDO::PARAM_INT);
+            $stmt->execute();
+            $db = null;
+            $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            // remover nulls e strings vazias
+            $dados = array_filter(array_map(function ($evento) {
+                return $evento = array_filter($evento, function ($coluna) {
+                    return $coluna !== null && $coluna !== '';
+                });
+            }, $dados));
+
+            $dadosLength = (int)sizeof($dados);
+            if ($dadosLength === 0) {
+                $dados = ["error" => 'pagina inexistente'];
+                // Page not found
+                $responseData = [
+                    "status" => 404,
+                    "info" => 'pagina inexistente'
+                ];
+
+            } else if ($dadosLength < $results) {
+                $responseData = [
+                    "status" => 200,
+                    "data" => $dados,
+                    "info" => "final dos resultados"
+                ];
+            } else {
+                $nextPageUrl = Helper::nextPageUrl();
+                $responseData = [
+                    "status" => 200,
+                    "data" => $dados,
+                    "proxPagina" => "$nextPageUrl?page=" . ++$page . "&results=$results"
+                ];
+            }
+
+            return $response
+                ->withJson($responseData, $status, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
+
+
+        } catch (PDOException $err) {
+            $status = 503; // Service unavailable
+            // Primeiro callback chamado em ambiente de desenvolvimento, segundo em producao
+            $errorMsg = Errors::filtroReturn(function ($err) {
+                return [
+
+                    "status" => $err->getCode(),
+                    "info" => $err->getMessage()
+
+                ];
+            }, function () {
+                return [
+                    "status" => 503,
+                    "info" => 'Servico Indisponivel'
+                ];
+            }, $err);
+
+            return $response
+                ->withJson($errorMsg, $status, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
+        }
+
+
+
+    }else {
+        $status = 422; // Unprocessable Entity
+        $errorMsg = [
+            "status" => "$status",
+            "info" => 'Parametros invalidos'
+
+        ];
+
+        return $response
+            ->withJson($errorMsg, $status, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_HEX_QUOT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
+    }
+});
